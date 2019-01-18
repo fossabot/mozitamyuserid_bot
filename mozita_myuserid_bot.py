db9d3059a46dfbb018197aad728a572ecf3ca389
Saverio Morelli <saverio.morelli@outlook.it>
2019-01-18 21:52:33 +0100

    aggiornamento a 1.0.1
    - Implementato sistema di token
    - Corretto problema CRLF to LF
    - Aggiunto salvataggio anche delle chatid

--- mozita_myuserid_bot.py
+++ mozita_myuserid_bot.py
@@ -3,13 +3,25 @@ import time
 from telepot.loop import MessageLoop
 import json
 from pathlib import Path
+import os
+from configparser import ConfigParser
 
-TOKEN="---NASCOSTO---"
+if not os.path.isfile("config.ini"):
+    print("Il file di configurazione non è presente. Rinomina il file 'config-sample.ini' in 'config.ini' e inserisci il token.")
+    exit()
 
-#COPIARE E INCOLLARE DA QUI - IL TOKEN E' GIA' INSERITO
+script_path = os.path.dirname(os.path.realpath(__file__))
+config_parser = ConfigParser()
+config_parser.read(os.path.join(script_path,"config.ini"))
 
-versione="1"
-ultimoAggiornamento="24-11-2018"
+TOKEN=config_parser.get("access","token")
+
+if TOKEN == "":
+    print("Token non presente.")
+    exit()
+
+versione="1.0.1"
+ultimoAggiornamento="18-01-2019"
 
 def risposte(msg):
     type_msg="NO"
@@ -28,17 +40,28 @@ def risposte(msg):
     if type_msg=="OK":
         userid=[]
         userid_path="userid_list.json"
+        chatid=[]
+        chatid_path="chatid_list.json"
         if Path(userid_path).exists():
             userid = json.loads(open(userid_path).read())
+        if Path(chatid_path).exists():
+            chatid = json.loads(open(chatid_path).read())
         bot.sendMessage(chat_id, "Il tuo userid è: "+str(user_id))
         print(f"Il tuo userid è: {user_id}")
         if not int(user_id) in userid:
             userid.append(int(user_id))
+        if not int(chat_id) in chatid:
+            chatid.append(int(chat_id))
         try:
             with open(userid_path, "wb") as f:
                 f.write(json.dumps(userid).encode("utf-8"))
         except Exception as e:
             print("Excep:01 -> "+str(e))
+        try:
+            with open(chatid_path, "wb") as f:
+                f.write(json.dumps(chatid).encode("utf-8"))
+        except Exception as e:
+            print("Excep:02 -> "+str(e))
 
 bot=telepot.Bot(TOKEN)
 MessageLoop(bot, {'chat': risposte, 'callback_query': risposte}).run_as_thread()
